5dc7f3948dd995bf79ed977abd84c213b750bbf1
Alex Symonds <alex-symonds@gmail.com>
2022-09-12 15:46:46 +0100

    Added PythonAnywhere to ALLOWED_HOSTS

--- adminas-project/settings.py
+++ adminas-project/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['alexsymonds.pythonanywhere.com']
 
 CSRF_COOKIE_SECURE = True
 SESSION_COOKIE_SECURE = True
