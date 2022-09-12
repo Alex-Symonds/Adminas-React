d4bcecba75b0fc8097f3f8eccbc1349ea0524b0d
Alex Symonds <alex-symonds@gmail.com>
2022-09-12 16:31:28 +0100

    Added scripts to repo

--- adminas-project/settings.py
+++ adminas-project/settings.py
@@ -127,11 +127,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
-STATIC_ROOT = '/adminas/static/'
-MEDIA_ROOT = '/adminas/media/'
+STATIC_ROOT = '/home/alexsymonds/Adminas-React/adminas/static/'
+MEDIA_ROOT = '/home/alexsymonds/Adminas-React/adminas/media/'
 
-if DEBUG == True:
-    STATIC_ROOT = 'Z:/Documents/Programming/adminas-react-gulp/adminas/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
